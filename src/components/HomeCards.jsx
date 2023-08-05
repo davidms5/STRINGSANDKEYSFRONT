cebe692ed2a28bd2728fd4443cceea8b7101c3de
Martin Fuks <martinfukss@gmail.com>
2023-08-05 02:00:26 -0300

    Estilos Home, Carrousel, Cards, Footer, ads

---
+++ src/components/HomeCards.jsx
@@ -0,0 +1,166 @@
+import {
+  Box,
+  Link,
+  SimpleGrid,
+  Text,
+  Image,
+  Heading,
+  textDecoration,
+} from "@chakra-ui/react";
+
+export default function CategoryCards() {
+  return (
+    <Box h={"800px"} w={"1300px"} align={"center"}>
+      <SimpleGrid columns={5} row={2} spacing={20}>
+        <Box bg="#ffa200" height="200px" w={"200px"} rounded={"10px"}>
+          <Text
+            mt={"15px"}
+            h={"45px"}
+            color={"black"}
+            fontSize={"20px"}
+            fontWeight={"700"}
+          >
+            Afinadores
+          </Text>
+          <Link>
+            <Image src="https://res.cloudinary.com/dlmr7znxt/image/upload/v1690928515/StringsAndKeys/1001_rx2ca0.jpg"></Image>
+          </Link>
+        </Box>
+        <Box bg="#ffa200" height="200px" w={"200px"} rounded={"10px"}>
+          <Text
+            mt={"15px"}
+            h={"45px"}
+            color={"black"}
+            fontSize={"20px"}
+            fontWeight={"700"}
+          >
+            Correas
+          </Text>
+          <Link>
+            <Image src="https://res.cloudinary.com/dlmr7znxt/image/upload/v1690928515/StringsAndKeys/1005_piq59k.jpg"></Image>
+          </Link>
+        </Box>
+        <Box bg="#ffa200" height="200px" w={"200px"} rounded={"10px"}>
+          <Text
+            mt={"15px"}
+            h={"45px"}
+            color={"black"}
+            fontSize={"20px"}
+            fontWeight={"700"}
+          >
+            Fundas
+          </Text>
+          <Link>
+            <Image src="https://res.cloudinary.com/dlmr7znxt/image/upload/v1690928515/StringsAndKeys/1011_hynib2.jpg"></Image>
+          </Link>
+        </Box>
+        <Box bg="#ffa200" height="200px" w={"200px"} rounded={"10px"}>
+          <Text
+            mt={"15px"}
+            h={"45px"}
+            color={"black"}
+            fontSize={"20px"}
+            fontWeight={"700"}
+          >
+            Púas
+          </Text>
+          <Link>
+            <Image src="https://res.cloudinary.com/dlmr7znxt/image/upload/v1690928516/StringsAndKeys/1013_w6vdpp.jpg"></Image>
+          </Link>
+        </Box>
+        <Box bg="#ffa200" height="200px" w={"200px"} rounded={"10px"}>
+          <Text
+            mt={"15px"}
+            h={"45px"}
+            color={"black"}
+            fontSize={"20px"}
+            fontWeight={"700"}
+          >
+            Cables
+          </Text>
+          <Link>
+            <Image src="https://res.cloudinary.com/dlmr7znxt/image/upload/v1690928516/StringsAndKeys/1016_sbqdew.jpg"></Image>
+          </Link>
+        </Box>
+        <Box bg="#ffa200" height="200px" w={"200px"} rounded={"10px"}>
+          <Text
+            mt={"15px"}
+            h={"45px"}
+            color={"black"}
+            fontSize={"20px"}
+            fontWeight={"700"}
+          >
+            Teclados
+          </Text>
+          <Link>
+            <Image src="https://res.cloudinary.com/dlmr7znxt/image/upload/v1690928517/StringsAndKeys/1021_kodwap.jpg"></Image>
+          </Link>
+        </Box>
+        <Box bg="#ffa200" height="200px" w={"200px"} rounded={"10px"}>
+          <Text color={"black"} fontSize={"20px"} fontWeight={"700"}>
+            Instrumentos de Cuerda
+          </Text>
+          <Link>
+            <Image src="https://res.cloudinary.com/dlmr7znxt/image/upload/v1690928517/StringsAndKeys/1026_wht8zz.jpg"></Image>
+          </Link>
+        </Box>
+        <Box bg="#ffa200" height="200px" w={"200px"} rounded={"10px"}>
+          <Text color={"black"} fontSize={"20px"} fontWeight={"700"}>
+            Instrumentos de Viento
+          </Text>
+          <Link>
+            <Image src="https://res.cloudinary.com/dlmr7znxt/image/upload/v1690928519/StringsAndKeys/1043_roo9gt.jpg"></Image>
+          </Link>
+        </Box>
+        <Box bg="#ffa200" height="200px" w={"200px"} rounded={"10px"}>
+          <Text
+            mt={"15px"}
+            h={"45px"}
+            color={"black"}
+            fontSize={"20px"}
+            fontWeight={"700"}
+          >
+            Amplificadores
+          </Text>
+          <Link>
+            <Image src="https://res.cloudinary.com/dlmr7znxt/image/upload/v1690928520/StringsAndKeys/1049_qfmpgg.jpg"></Image>
+          </Link>
+        </Box>
+        <Box bg="#ffa200" height="200px" w={"200px"} rounded={"10px"}>
+          <Text
+            mt={"15px"}
+            h={"45px"}
+            color={"black"}
+            fontSize={"20px"}
+            fontWeight={"700"}
+          >
+            Baterías y Percusión
+          </Text>
+          <Link>
+            <Image src="https://res.cloudinary.com/dlmr7znxt/image/upload/v1690928520/StringsAndKeys/1052_dvhx0o.jpg"></Image>
+          </Link>
+        </Box>
+        <Link
+          _hover={{
+            textDecoration: "none",
+          }}
+        >
+          <Box
+            mt={"50px"}
+            bg={"black"}
+            w={"1305px"}
+            h={"100px"}
+            rounded={"10px"}
+          >
+            <Heading pt={"25px"} color={"white"}>
+              TODOS NUESTROS PRODUCTOS
+            </Heading>
+          </Box>
+        </Link>
+      </SimpleGrid>
+      <br />
+      <br />
+      <Image h={"300px"} src="/ads.png"></Image>
+    </Box>
+  );
+}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -35,12 +35,14 @@ export default function WithSubnavigation() {
         color={useColorModeValue("gray.600", "white")}
         minH={"60px"}
         h={'100px'}
+        w={'100%'}
         py={{ base: 2 }}
         px={{ base: 4 }}
         borderBottom={1}
         borderStyle={"solid"}
         borderColor={useColorModeValue("gray.200", "gray.900")}
         align={"center"}
+        position="fixed"
       >
         <Flex
           flex={{ base: 1, md: "auto" }}
