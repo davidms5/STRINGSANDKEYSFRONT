a6547a54dad859e2a0be802d9c3a02f48148edd8
MatyDiaz <97753015+MatyDiaz@users.noreply.github.com>
2023-08-10 18:57:41 -0300

    filtro brand,action y reducer

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,1 +1,27 @@
 import axios from "axios";
+
+export const GET_PRODUCTS = 'GET_PRODUCTS';
+export const FILTER_BRAND = 'FILTER_BRAND';
+
+const LOCAL_HOST = import.meta.env.LOCAL_HOST;
+
+export const getProducts = () => {
+    return async function(dispatch) {
+        const response = await axios.get(`${LOCAL_HOST}/products`);
+        const products = response.data;
+
+        dispatch({
+            type:GET_PRODUCTS,
+            payload:products
+        })
+    };
+};
+
+export const filterBrand = (brand) => {
+    return {
+        type: FILTER_BRAND,
+        payload: brand,
+    }
+};
+
+
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,20 +1,34 @@
 import { persistReducer } from "redux-persist";
 import storageSession from "redux-persist/lib/storage/session";
 import storage from "redux-persist/lib/storage";
+import { FILTER_BRAND } from "./actions";
 const persistConfig = {
   key: "root",
   //storage: storageSession,
   storage, //esta parte y la de arriba es para usar el localStorage en ves de la ssesion
 };
 
-
-const initialState = {}
-
+const initialState = {
+  products: [],
+  filteredProducts: [],
+};
 
 const rootReducer = (state = initialState, action) => {
-    switch (action.type) {}
-}
+  switch (action.type) {
+    case FILTER_BRAND:
+      const productsByBrand =
+        action.payload === "todos"
+          ? [...state.products]
+          : [...state.products].filter((el) =>
+              el.brand?.some((e) => e === action.payload)
+            );
 
+      return {
+        ...state,
+        filteredProducts: productsByBrand,
+      };
+  }
+};
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
