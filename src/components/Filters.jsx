df8794a9cf019f248db82a0dd0f0abd19fd63023
Martin Fuks <martinfukss@gmail.com>
2023-08-10 21:42:06 -0300

    Card, CardsContainer, Paginado

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,14 +1,25 @@
 import { Box, Flex } from "@chakra-ui/react";
 import { filterBrand } from "../redux/actions";
 
+import { useDispatch, useSelector } from "react-redux";
 
 const FilterAndOrder = () => {
 
+  const dispatch = useDispatch();  
+  const filteredProducts = useSelector((state) => state.filteredProducts);
+
+  const brands = filteredProducts.map(el=>el.brand);
+
+  const handleBrandFilter = (e) => {
+    const selectedBrand = e.target.value;
+
+    dispatch(filterBrand(selectedBrand));
+    
+  };
 
   return (
     <Box>
-      <Flex>
-      </Flex>
+      <Flex></Flex>
     </Box>
   );
 };
